ac1472c3a29199512ab5ce23c5908d073c3c4039
Noobie III <109138085+Pochinki98@users.noreply.github.com>
2025-12-05 11:21:43 +0800

    Fix video URL parameter in player.js

--- player.js
+++ player.js
@@ -1,5 +1,5 @@
   var video = document.getElementById('video');
-  var vurl = location.search.split('vurl=')[1];
+  var vurl = location.search.split('url=')[1];
   var videoSrc = vurl;
   // 检查浏览器是否支持hls
   if (video.canPlayType('application/vnd.apple.mpegurl')) {
@@ -10,3 +10,4 @@
     hls.loadSource(videoSrc);
     hls.attachMedia(video);
   }
+
